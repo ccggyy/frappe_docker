8cb5993c6e9c5cc60071e42c919a9335da8d1949
Chakery <chakerychen@gmail.com>
2023-06-18 15:41:29 +0800

    custom app

--- build-custom-image.sh
+++ build-custom-image.sh
@@ -19,6 +19,10 @@ export APPS_JSON_BASE64=$(echo ${APPS_JSON} | base64 -w 0)
 
 docker build \
   --build-arg=PYTHON_VERSION=3.10.12 \
+  --build-arg=WKHTMLTOPDF_VERSION=0.12.6-1 \
+  --build-arg=NODE_VERSION=16.18.0 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
+  --build-arg=FRAPPE_BRANCH=version-14 \
+  --build-arg=FRAPPE_PATH=https://github.com/ccggyy/frappe \
   --tag=ccggyy/erpnext:0.0.1 \
   --file=images/custom/Containerfile .
